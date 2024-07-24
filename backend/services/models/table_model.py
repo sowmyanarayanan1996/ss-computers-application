132a2616c2f99f6afab144672f6c5e2ef9224fb6
sowmyanarayanan <sowmyanarayanan1996@gmail.com>
2024-07-24 08:02:05 +0530

    total enquiry and application

--- backend/services/models/table_model.py
+++ backend/services/models/table_model.py
@@ -7,9 +7,21 @@ from sqlalchemy.sql.expression import text
 
 class EnquiryEntity(Base):
     __tablename__ = 'enquiry_entity'
-    enquiry_id = Column(UUID(as_uuid=True),default=uuid.uuid4, nullable=True)
-    enquiry_name = Column(String,unique=True,nullable=False,primary_key=True)
-    description = Column(String,nullable=False)
-    mobile_number = Column(String,nullable=False)
-    course=Column(String,nullable=False)
+    enquiry_id = Column(type_=UUID(as_uuid=True),default=uuid.uuid4, nullable=True)
+    enquiry_name = Column(type_=String,unique=True,nullable=False,primary_key=True)
+    description = Column(type_=String,nullable=False)
+    mobile_number = Column(type_=String,nullable=False)
+    course=Column(type_=String,nullable=False)
+    email_address= Column(type_=String,nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
+
+
+class AdmissionEntity(Base):
+    __tablename__ = 'admission_entity'
+    admission_id = Column(type_=UUID(as_uuid=True),default=uuid.uuid4, nullable=True)
+    candidate_name = Column(type_=String,unique=True,nullable=False,primary_key=True)
+    description = Column(type_=String,nullable=False)
+    mobile_number = Column(type_=String,nullable=False)
+    course=Column(type_=String,nullable=False)
+    email_address= Column(type_=String,nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
--- backend/services/routers/router.py
+++ backend/services/routers/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter,Depends
-from schemas.schemas import Enquiry
+from schemas.schemas import Enquiry,Admission
 from sqlalchemy.orm import session as dbsession
 from databases.database import get_db
 import models.table_model as models
@@ -8,31 +8,96 @@ ss_computer_router = APIRouter()
 
 
 
-@ss_computer_router.post('/enquiry/create')
-def enquiry(details:Enquiry,db:dbsession=Depends(get_db)):
+@ss_computer_router.post('/enquiry/create',tags=["Enquiry"])
+def create_enquiry(details:Enquiry,db:dbsession=Depends(get_db)):
     try:
         details_dict = details.dict()
         new_row = models.EnquiryEntity(**details_dict)
-        enquiry_details = db.query(models.EnquiryEntity).filter(models.EnquiryEntity.enquiry_name==details.enquiry_name,models.EnquiryEntity.description==details.description,models.EnquiryEntity.mobile_number==details.mobile_number,models.EnquiryEntity.course==details.course)
+        enquiry_details = db.query(models.EnquiryEntity).filter(models.EnquiryEntity.enquiry_name==details.enquiry_name,models.EnquiryEntity.description==details.description,models.EnquiryEntity.mobile_number==details.mobile_number,models.EnquiryEntity.course==details.course,models.EnquiryEntity.email_address==details.email_address)
         if (not enquiry_details.count()):
             db.add(new_row)
             db.commit()
             db.refresh(new_row)
             return{"status code":"200","message":"successfully added the enquiry details"}
         else:
-            return{"status code":"401","message":"enquiry name already present","other details":new_row}
+            return{"status code":"401","message":"enquiry name already present","enquired details":new_row}
        
     except Exception as e:
         return{"status code":"400","message":"Enquiry created failed","Error":repr(e)}
     
-@ss_computer_router.get('/enquiry/retrieve')
+@ss_computer_router.get('/enquiry/retrieve',tags=["Enquiry"])
 def retrieve_enquiry(db:dbsession=Depends(get_db)):
     try:
-        rows = db.query(models.EnquiryEntity.enquiry_name,models.EnquiryEntity.enquiry_name,models.EnquiryEntity.description,models.EnquiryEntity.mobile_number,models.EnquiryEntity.course,models.EnquiryEntity.enquiry_id,models.EnquiryEntity.created_at)
+        rows = db.query(models.EnquiryEntity.enquiry_name,models.EnquiryEntity.enquiry_name,models.EnquiryEntity.description,models.EnquiryEntity.mobile_number,models.EnquiryEntity.course,models.EnquiryEntity.email_address,models.EnquiryEntity.enquiry_id,models.EnquiryEntity.created_at)
         if (rows.count()):
-            return{"status code":"200","message":"successfully retrieve all project","details":rows.all()}
+            return{"status code":"200","message":"successfully retrieve all equiries","details":rows.all()}
         else:
             return{"status code":"401","message":"No existing enquiry","response body":[]}
         
     except Exception as e:
         return {"status code":"400","message":"Enquiry failed to fetch","other details":repr(e)}
+    
+@ss_computer_router.delete('/enquiry/delete',tags=["Enquiry"])
+def delete_enquiry(details:Enquiry,db:dbsession=Depends(get_db)):
+        try:
+    
+            
+            enquiry_delete_details = db.query(models.EnquiryEntity).filter(models.EnquiryEntity.enquiry_name==details.enquiry_name,models.EnquiryEntity.mobile_number==details.mobile_number)
+            if (enquiry_delete_details.count()):
+                enquiry_delete_details.delete(synchronize_session=False)
+                db.commit()
+                
+                return{"status code":"200","message":"successfully deleted the enquiry details"}
+            else:
+                return{"status code":"401","message":"There is no enquiry details with this name, please add the enquiry details"}
+       
+        except Exception as e:
+            return{"status code":"400","message":"Enquiry created failed","Error":repr(e)}
+
+
+@ss_computer_router.post('/admission/create',tags=['Admission'])
+def create_admission(details:Admission,db:dbsession=Depends(get_db)):
+    try:
+        details_dict = details.dict()
+        new_row = models.AdmissionEntity(**details_dict)
+        admission_details = db.query(models.AdmissionEntity).filter(models.AdmissionEntity.candidate_name==details.candidate_name,models.AdmissionEntity.description==details.description,models.AdmissionEntity.mobile_number==details.mobile_number,models.AdmissionEntity.course==details.course,models.AdmissionEntity.email_address==details.email_address)
+        if (not admission_details.count()):
+            db.add(new_row)
+            db.commit()
+            db.refresh(new_row)
+            return{"status code":"200","message":"successfully added the admission details"}
+        else:
+            return{"status code":"401","message":"admission name already present","admission details":new_row}
+       
+    except Exception as e:
+        return{"status code":"400","message":"Admission created failed","Error":repr(e)}
+    
+
+@ss_computer_router.get('/admission/retrieve',tags=["Admission"])
+def retrieve_admission(db:dbsession=Depends(get_db)):
+    try:
+        rows = db.query(models.AdmissionEntity.candidate_name,models.AdmissionEntity.description,models.AdmissionEntity.mobile_number,models.AdmissionEntity.course,models.AdmissionEntity.email_address,models.AdmissionEntity.admission_id,models.AdmissionEntity.created_at)
+        if (rows.count()):
+            return{"status code":"200","message":"successfully retrieve all admission","details":rows.all()}
+        else:
+            return{"status code":"401","message":"No existing admission","response body":[]}
+        
+    except Exception as e:
+        return {"status code":"400","message":"Admission failed to fetch","other details":repr(e)}
+    
+@ss_computer_router.delete('/admission/delete',tags=["Admission"])
+def delete_admission(details:Admission,db:dbsession=Depends(get_db)):
+        try:
+    
+            
+            enquiry_delete_details = db.query(models.AdmissionEntity).filter(models.AdmissionEntity.candidate_name==details.candidate_name,models.AdmissionEntity.mobile_number==details.mobile_number)
+            if (enquiry_delete_details.count()):
+                enquiry_delete_details.delete(synchronize_session=False)
+                db.commit()
+                
+                return{"status code":"200","message":"successfully deleted the enquiry details"}
+            else:
+                return{"status code":"401","message":"There is no enquiry details with this name, please add the enquiry details"}
+       
+        except Exception as e:
+            return{"status code":"400","message":"Enquiry created failed","Error":repr(e)}
